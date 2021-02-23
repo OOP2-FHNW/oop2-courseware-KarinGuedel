1454ae649371d9aedad547a488b1ec40d5b1c215
KarinGuedel <75257800+KarinGuedel@users.noreply.github.com>
2021-02-23 15:06:41 +0100

    Create PersonTest.java

---
+++ src/test/java/oop2/module01/PersonTest.java
@@ -0,0 +1,12 @@
+package oop2.module01;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class PersonTest {
+    @Test
+    public void testInitialization() {
+
+    }
+}
