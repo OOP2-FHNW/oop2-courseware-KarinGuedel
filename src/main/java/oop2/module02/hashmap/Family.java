c9cc51f46028af1fc17e11ad8723018b4a7c891c
KarinGuedel <75257800+KarinGuedel@users.noreply.github.com>
2021-03-07 22:03:47 +0100

    nächster Schritt

---
+++ src/main/java/oop2/module02/hashmap/Family.java
@@ -0,0 +1,61 @@
+package oop2.module02.hashmap;
+
+import java.util.*;
+
+import oop2.module02.Person;
+
+/**
+ * @author Dieter Holz
+ */
+public class Family {
+    private final Set<Person> members = new HashSet<>();
+
+    private final Map<Integer, Integer> ageToCountMap = new HashMap<>();
+
+
+    public int size() {
+        return members.size();
+    }
+
+    public void add(Person person) {
+        members.add(person);
+
+        Integer count = ageToCountMap.get(person.getAge());
+        if(count == null){
+            ageToCountMap.put(person.getAge(), 1);
+        }
+        else {
+            ageToCountMap.put(person.getAge(), count + 1);
+        }
+    }
+
+    public Person getOldest() {
+        Person oldest = null;
+        for (Person p : members) {
+            if (oldest == null || p.getAge() > oldest.getAge()) {
+                oldest = p;
+            }
+        }
+        return oldest;
+    }
+
+    public boolean isMember(Person person) {
+        return members.contains(person);
+    }
+
+    public int numberOfMembers(int age) {
+        Integer count = ageToCountMap.get(age);
+        if(count == null){
+            return 0;
+        }
+        else {
+            return count;
+        }
+        //return count == null ? 0 : count;
+    }
+
+    public Set<Integer> allAges() {
+        return ageToCountMap.keySet();
+    }
+
+}
